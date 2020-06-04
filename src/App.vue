d41e7e02cd9426bcddcb635e70997cf91ae1828c
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-04 12:54:48 +0200

    Add New Quote

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="container">
+    <AppNewQuote @quotedAdded="NewQuote"></AppNewQuote>
+    <AppQuoteGrid :quotes="quotes"></AppQuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click pn a Quote to delete it! </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import QuoteGrid from './component/QuoteGrid';
+import NewQuote from './component/NewQuote';
   export default{
-    
+    data:function(){
+      return {
+        quotes: ['Just a Quote to start'],
+        maxQuotes : 10
+      }
+    },
+    components:{
+      AppQuoteGrid : QuoteGrid,
+      AppNewQuote:NewQuote
+    },
+    methods:{
+      NewQuote(event){
+        this.quotes.push(event);
+      }
+    }
   }
 </script>
 
---
+++ src/component/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <AppQuote v-for="quote in quotes">{{ quote }} </AppQuote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+    export default {
+        props:['quotes'],
+        components: {
+            AppQuote : Quote
+        }
+    }
+</script>
+
+<style>
+
+</style>
